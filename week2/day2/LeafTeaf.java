fe8b0e349e9632f23639c71687e85c73ded1571a
Aishwarya0217 <126107932+Dheeksha0217@users.noreply.github.com>
2023-02-28 23:56:14 +0530

    Add files via upload

---
+++ week2/day2/LeafTeaf.java
@@ -0,0 +1,19 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LeafTeaf {
+	
+	public static void main(String[] args) {
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
+
+}
